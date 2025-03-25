6f883c9307ae631cf8836f4a9a5f8a77933f2c75
ojasvsingh71 <ojasvsingh0@gmail.com>
2025-03-25 11:57:05 +0530

    backend

--- sheguard-backend/app.py
+++ sheguard-backend/app.py
@@ -73,4 +73,6 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
     app.run(debug=True)
